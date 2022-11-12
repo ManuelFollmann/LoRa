39d81f0604b4e8c8d2e7edcb9d5cf0dc145ac703
ManuelFollmann <manuel.follmann@web.de>
2022-11-12 12:41:55 +0100

    Add files via upload

---
+++ M5_Stack_BME680/src/ttn.h
@@ -0,0 +1,10 @@
+// DevEUI TTN console V3
+// MSB first
+char* DevEui="70B3D57ED004F501";
+
+// AppEUI TTN console V3
+char* AppEui="0000000000000000";   
+
+// AppKey TTN console V3
+// MSB first
+char* AppKey="1D8E0C86CD6ECCE30629333EF5A5EE7F";
